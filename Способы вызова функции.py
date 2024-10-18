935de46040203c85f5e3399dc14b8e982b6d50e7
Whale-99 <168347567+Whale-99@users.noreply.github.com>
2024-10-19 00:34:43 +0300

    Update Способы вызова функции.py

--- Способы вызова функции.py
+++ Способы вызова функции.py
@@ -1,24 +1,22 @@
 recipient = input("Введите получателя: ")
 message = input("Введите письмо: ")
 sender = input("Введите отправителя (по умолчанию: university.help@gmail.com): ")
-
 def send_email(message, recipient, sender="university.help@gmail.com"):
+    valid_domains = [".com", ".ru", ".net"]
+
+    if not (any(recipient.endswith(domain) for domain in valid_domains) and "@" in recipient):
+        print("Неверный адрес получателя")
+        return
+
+    if not (any(sender.endswith(domain) for domain in valid_domains) and "@" in sender):
+        print("Неверный адрес отправителя")
+        return
+
     print(f"Отправлено сообщение: {message}")
     print(f"Получатель: {recipient}")
     print(f"Отправитель: {sender}")
 
-print()
+if not sender:
+    sender = "university.help@gmail.com"
 
-valid_domains = [".com", ".ru", ".net"]
-
-
-if (any(recipient.endswith(domain) for domain in valid_domains)) and "@" in recipient:
-    if sender:
-        if (any(sender.endswith(domain) for domain in valid_domains)) and "@" in sender:
-            send_email(message, recipient, sender)
-        else:
-            print("Неверный адрес отправителя")
-    else:
-        send_email(message, recipient)
-else:
-    print("Неверный адрес получателя")
+send_email(message, recipient, sender)
